84d773977696dba8adb7914ace7916fa5ea6b988
João Pedro Portela Gómez Vaz <jpvaz@br.ibm.com>
2015-10-06 21:38:33 -0300

    Parse Gunpla
    - Changed the default method of structuring data from list to
    dictionary.
    - All fields but ‘name’ are optional.
    - Grade included.

--- gunpla.py
+++ gunpla.py
@@ -2,18 +2,59 @@ import re
 import datetime
 
 class Gunpla(object):
-    def __init__(self, gunpla_id, lst):
+
+    def __init__(self, gunpla_id, product):
         self.id = gunpla_id
-        self.name = re.sub(r'\([^)]*\)', '', lst[0])
-        self.manufacture = lst[1]
-        self.scale = lst[2]
-        self.series = lst[3]
-        self.release_date = re.sub(r'^\W*\w+\W*', '', lst[4]).replace('.', '')
-        self.release_date_code = datetime.datetime.strptime(
-                                 self.release_date,
-                                 '%b, %Y')
+        self.name = re.sub(r'\([^)]*\)', '', product['name'])
+
+        if 'manufacture' in product:
+            self.manufacture = product['manufacture']
+        else:
+            self.manufacture = ''
+
+        if 'grade' in product:
+            self.grade = product['grade']
+        else:
+            self.grade = product['grade']
+
+        if 'scale' in product:
+            if product['scale'] == 'Non':
+                self.scale = 'Not applicable '
+            else:
+                self.scale = product['scale']
+        else:
+            self.scale = ''
+
+        if 'series' in product:
+            self.series = product['series']
+        else:
+            self.series = ''
+
+        if 'release date' in product:
+            if product['release date'] != 'Pre-Order':
+                self.release_date = re.sub(r'^\W*\w+\W*', '',
+                                    product['release date']).replace('.', '')
+            else:
+                self.release_date = 'Not yet released'
+        else:
+            self.release_date = ''
+
+        try:
+            self.release_date_code = datetime.datetime.strptime(
+                                    self.release_date,
+                                    '%b, %Y')
+        except:
+            self.release_date_code = None
+
+        self.name = self.name.upper()
+        self.manufacture = self.manufacture.upper()
+        self.scale = self.scale.upper()
+        self.series = self.series.upper()
+        self.release_date = self.release_date.upper()
 
     def __str__(self):
-        return ('ID: {} \nNAME: {} \nMANUFACTURE: {} \nSCALE: {} \n'
-               'SERIES: {} \nRELEASE DATE: {}'.format(self.id, self.name,
-               self.manufacture, self.scale, self.series, self.release_date))
+        return ('ID: {} \nNAME: {} \nGRADE: {}\nMANUFACTURE: {} \n'
+                'SCALE: {} \nSERIES: {} \nRELEASE DATE: {}\n'
+                'DATE: {}'.format(self.id, self.name, self.grade,
+                self.manufacture, self.scale, self.series,
+                self.release_date, self.release_date_code))
--- parse1999.py
+++ parse1999.py
@@ -5,29 +5,58 @@ from parser import LinksParser
 import requests
 
 
-def parse_gunpla(lst):
+def parse_gunpla():
+    preorder = False
+    lst = list()
     for item in parser.data:
         item = item.replace('\r\n\t\t\t\t', '').strip()
-        if item.strip() != '' and item.strip() != ':':
-                lst.append(item)
+
+        if item.strip() != '' and item.strip() != ':' and item.strip() != ',':
+            lst.append(item)
+
+    if 'Pre-order' in lst[-1]:
+        preorder = True
+
+    gunpla = {'name':lst[0]}
 
     for idx,item in enumerate(lst):
-        if idx % 2 != 0:
-            lst[idx] = ''
+        if lst[idx] == 'Manufacturer':
+            gunpla['manufacture'] = lst[idx+1]
+        elif lst[idx] == 'Scale':
+            if lst[idx+2] == 'Original':
+                gunpla['scale'] = lst[idx+1]
+            else:
+                gunpla['scale'] = lst[idx+2]
+        elif lst[idx] == 'Original':
+            gunpla['series'] = lst[idx+1]
+        elif lst[idx] == 'Release Date':
+            if not preorder:
+                gunpla['release date'] = lst[idx+1]
+            else:
+                gunpla['release date'] = 'Pre-Order'
+        else:
+            pass
 
-    while '' in lst:
-        lst.remove('')
+    gunpla['grade'] = ''
+    grades = ['FG', 'SD', 'HG', 'MG', 'RG', 'PG', 'RE/100']
 
+    for item in grades:
+        if '('+item in gunpla['name']:
+            gunpla['grade'] = item
+
+
+    return gunpla
 
 while(True):
     parser = LinksParser()
-    link = 'http://www.1999.co.jp/eng/10310668'
-    page = requests.get(link)
+    link = 'http://www.1999.co.jp/eng/10011577'
+    page = requests.get(link.strip())
     if page.status_code == 200:
         parser.feed(page.text)
-        lst = list()
-        parse_gunpla(lst)
+        product = parse_gunpla()
+        if not product:
+            break
         gunpla_id = link.split('/')[-1]
-        gunpla = Gunpla(gunpla_id, lst)
+        gunpla = Gunpla(gunpla_id, product)
         print(gunpla)
         break
